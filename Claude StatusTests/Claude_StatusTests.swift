779251e41c4541917035148ab82cc6d7875ed8c0
Gavin M. Roy <gavinr@aweber.com>
2026-03-10 16:59:14 -0400

    Initial Commit

---
+++ Claude StatusTests/Claude_StatusTests.swift
@@ -0,0 +1,17 @@
+//
+//  Claude_StatusTests.swift
+//  Claude StatusTests
+//
+//  Created by Gavin M Roy on 3/10/26.
+//
+
+import Testing
+@testable import Claude_Status
+
+struct Claude_StatusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
